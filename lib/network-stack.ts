7571d35ee8cbe562579dc89e7bc57f9d5ad171bc
”yourname” <s01t2w@gmail.com>
2019-12-17 14:46:27 +0900

    Implement S3 and CloudWatch Logs Endpoints

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -31,7 +31,7 @@ export class NetworkStack extends cdk.Stack {
       ]
     });
 
-    // Add a gateway endpoint on the VPC
+    // Add DynamoDB gateway endpoint on the VPC
     const dynamoDbEndpoint = this.vpc.addGatewayEndpoint('DynamoDbEndpoint', {
       service: ec2.GatewayVpcEndpointAwsService.DYNAMODB
     });
@@ -44,6 +44,19 @@ export class NetworkStack extends cdk.Stack {
         resources: ['*'],
     }));
 
+    // Add S3 gateway endpoint on the VPC
+    const S3Endpoint = this.vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3
+    });
+
+    // This allows to customize the endpoint policy
+    S3Endpoint.addToPolicy(
+      new iam.PolicyStatement({ // Restrict to listing and getting objects
+        principals: [new iam.AnyPrincipal()],
+        actions: ['s3:*'],
+        resources: ['*'],
+    }));
+
     // Add the ECR endpoint on the VPC
     const ecrDockerEndpoint = this.vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
       service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
@@ -53,8 +66,21 @@ export class NetworkStack extends cdk.Stack {
     // ecrDockerEndpoint.connections.allowDefaultPortFromAnyIpv4();
 
     // So, define a Security Group for the ECR endpoint.
-    const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "ECR Security Group", ecrDockerEndpoint.securityGroupId,);
-    securityGroup.addIngressRule(
+    const ecrsecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "ECR Security Group", ecrDockerEndpoint.securityGroupId,);
+    ecrsecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      "allow access from any IPs",
+    )
+
+    // Add the CloudWatch Logs endpoint on the VPC
+    const cloutwatchlogsEndpoint = this.vpc.addInterfaceEndpoint('CloudWatchLogsEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    });
+
+    // So, define a Security Group for the CloudWatch Logs endpoint.
+    const cwlsecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "CloudWatch Logs Security Group", cloutwatchlogsEndpoint.securityGroupId,);
+    cwlsecurityGroup.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
       "allow access from any IPs",
